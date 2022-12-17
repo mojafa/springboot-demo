5ef34d07d741fcbe82e37cd2f5a1b2ab8bd4de42
Jafa Mohammed <jmohsa@gmail.com>
2022-12-17 08:33:14 +0300

    Added repository- provides methods to access and manipulate the db

--- src/main/java/com/example/springdemo/repository/DemoRepository.java
+++ src/main/java/com/example/springdemo/repository/DemoRepository.java
@@ -1,2 +1,12 @@
-package com.example.springdemo.repository;public class DemoRepository {
+package com.example.springdemo.repository;
+
+import com.example.springdemo.model.Demo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface DemoRepository extends JpaRepository<Demo, Long> {
+    Demo findById(long id);
+    Demo deleteById(long id);
+    Demo findByName(String name);
 }
