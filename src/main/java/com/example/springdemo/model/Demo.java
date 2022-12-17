e8768231630133a1cfb8b32a3ca5e793692f3b5c
Jafa Mohammed <jmohsa@gmail.com>
2022-12-17 08:32:46 +0300

    Modified model

--- src/main/java/com/example/springdemo/model/Demo.java
+++ src/main/java/com/example/springdemo/model/Demo.java
@@ -1,2 +1,26 @@
-package com.example.springdemo.model;public class demo {
+package com.example.springdemo.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table (name = "users")
+public class Demo implements Serializable {
+@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // @Id
+   // @GeneratedValue(strategy = GenerationType.IDENTITY);
+  //  private Long id;
+    private  String name;
+    private  String phone;
+
+
 }
